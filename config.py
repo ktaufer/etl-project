95929580a3ae10f1e10ebd65313f798e5d9d84de
ktaufer <56054046+ktaufer@users.noreply.github.com>
2020-03-09 23:57:46 -0600

    yeah!

---
+++ config.py
@@ -0,0 +1,10 @@
+apiseeds_apikey = 'Fwk0Ln7pzcD8XFGcShLMzyqykTio9zJXlomg78tcHNDxNQ4kKVchi6K2K4e8ue4n'
+
+genius_client_id = 'JVvND0RB4Nrx7Q3gh77JyhHO5mh4TH-4csdZlS4iqomxGDsKFTt2n2f_8JO_8aWe'
+genius_client_secret = 'jkXKEoOFFRfnhy0YB5NKQgno_PPUo3Mrpf9GLhCMxjwG7OgSwGQ1QA2ig6lAh7P0HfAHypndXVIArCk0gPO1QQ'
+genius_client_access_token = 'S93UOwjpcJWM4jDo2KCo5zTZ9H-o371P6G0hMLET-7jRBm6HZHPWj4wlNKu6NQ8r'
+
+OAuth_Consumer_Key = '55a22ec5d2eacee9c4ab20f1c4c6727d1822365f'
+OAuth_Consumer_Secret = 'f3ad8587dfe4f668f1c0f9c1371d91ee3147c73e'
+OAuth_Access_Token = '0dc95570b2912b8f4151aa62042d9cff79ae5819'
+OAuth_Token_Secret = '76e18d82c3c4e8b7426b4a5e0138bce59ef552b1'
